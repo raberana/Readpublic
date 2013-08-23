f94ef6b7239fe164a355810435a7828e5f31cfb6
Roj Beraña <raberana@live.com>
2013-08-23 17:49:08 +0800

    fixed page transfer after signup added signin page

--- Readpublic/Angular/Scripts/Controllers/signupcontroller.js
+++ Readpublic/Angular/Scripts/Controllers/signupcontroller.js
@@ -1,21 +1,23 @@
 ﻿rpApp.controller('SignUpController', function ($scope, $location, userFactory) {
 
 
-    $scope.submit = function () {
-        var user = new userFactory({
-            UserName: $scope.user.username,
-            Password: $scope.user.password,
-            Email: $scope.user.email,
-            FirstName: $scope.user.firstname,
-            LastName: $scope.user.lastname,
-            MiddleName: $scope.user.middlename,
-        });
+	$scope.submit = function () {
+		var user = new userFactory({
+			UserName: $scope.user.username,
+			Password: $scope.user.password,
+			Email: $scope.user.email,
+			FirstName: $scope.user.firstname,
+			LastName: $scope.user.lastname,
+			MiddleName: $scope.user.middlename,
+		});
 
-        var res = user.$signup();
-        user.$promise.then(function (result) {
-            alert(result);
-            alert(angular.toJSON(result));
-        });
-    };
+		var res = user.$signup();
+
+		res.then(function (result) {
+			alert(result);
+			alert(angular.toJson(result));
+			$location.path('/Home');
+		});
+	};
 
 });
